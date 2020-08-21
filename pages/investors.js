5a6c6fabfab4243f8fdf90c904749a09466b9b6b
Sam Sverko <sam.sverko@gmail.com>
2020-08-21 15:17:57 -0400

    Investors - Build FinancialInfo section. - Add image asset.

--- pages/investors.js
+++ pages/investors.js
@@ -1,14 +1,20 @@
 import dynamic from 'next/dynamic'
 
 const Hero = dynamic(() => import('../components/investors/hero'))
-const InvestingFuture = dynamic(() => import('../components/investors/investing-future'))
+const InvestingFuture = dynamic(() =>
+  import('../components/investors/investing-future')
+)
+const FinancialInfo = dynamic(() =>
+  import('../components/investors/financial-info')
+)
 
 const Investors = () => {
   return (
     <>
       <Hero />
       <InvestingFuture />
+      <FinancialInfo />
     </>
-    )
+  )
 }
 export default Investors
