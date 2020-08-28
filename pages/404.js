e04323889325982dd07668889fea14fd33e571fb
Sam Sverko <sam.sverko@gmail.com>
2020-08-28 13:05:41 -0400

    404 - Build page. - Remove unused Visilink page.

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Content from '../components/404/content'
+
+export default function Error404() {
+  return (
+    <>
+      <Content />
+    </>
+  )
+}
--- pages/visilink.js
+++
@@ -1,3 +0,0 @@
-export default function Visilink() {
-  return <h1>Visilink page.</h1>
-}
