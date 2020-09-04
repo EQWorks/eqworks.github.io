636a15dea47beb2a6419c0bbaf946fc73337da30
Sam Sverko <sam.sverko@gmail.com>
2020-09-04 15:30:48 -0400

    Case Study - Build view. - Fix blockquote component styling.

--- pages/case-study/[slug].js
+++ pages/case-study/[slug].js
@@ -5,127 +5,166 @@ import styled from 'styled-components'
 
 import { getEntries, getEntryBySlug } from '../../api/contentful'
 
-import Date from '../../components/shared/parse-date'
 import ArticleContent from '../../components/shared/article-content'
-import ReadingTime from '../../components/press-release/reading-time'
 
 const PageStyled = styled.section`
-  .press-releases-link {
-    color: ${({ theme }) => theme.color.black};
-    cursor: pointer;
-    display: block;
-    font-size: 0.9em;
-    margin: 40px auto;
-    text-align: center;
-    text-decoration: none;
-    transition: color 0.25s ease-out;
-    &:hover {
-      color: ${({ theme }) => theme.color.navBarLinksListHover};
-    }
-  }
-`
-
-const TempPageTopPadding = styled.div`
-  background-color: ${({ theme }) => theme.color.black};
-  height: 100px;
-`
-
-const Article = styled.div`
-  color: ${({ theme }) => theme.color.greyLight};
   margin: 0 auto;
-  max-width: ${({ theme }) => theme.width.article};
-  padding: 0 20px 40px 20px;
-  h1 {
-    color: ${({ theme }) => theme.color.black};
-    font-size: 2em;
-    margin: 0 0 30px 0;
-    text-align: center;
-  }
-  .author-date {
-    color: ${({ theme }) => theme.color.black};
-    font-size: 0.9em;
-    margin: 0 0 30px 0;
-    text-align: center;
-  }
-  .content {
-    a {
-      color: ${({ theme }) => theme.color.black};
-      text-decoration: none;
-      transition: color 0.25s ease-out;
-      &:hover {
-        color: ${({ theme }) => theme.color.navBarLinksListHover};
+  max-width: ${({ theme }) => theme.width.page};
+  padding: 20px 0;
+  .header {
+    margin: 0 auto 20px auto;
+    max-width: ${({ theme }) => theme.width.article};
+    padding: 0 20px;
+    h1 {
+      font-size: 1.5em;
+      margin: 0 0 20px;
+      text-align: left;
+      @media ${({ theme }) => theme.breakpoint.sm} {
+        font-size: 2em;
+      }
+      @media ${({ theme }) => theme.breakpoint.md} {
+        font-size: 2.5em;
       }
     }
-    p {
-      margin: 0 0 20px 0;
-    }
-    ul {
-      margin: 0 0 20px 0;
-      li {
-        margin: 0 0 10px 0;
-        &:last-child {
-          margin: 0 0 0 0;
-        }
-        p {
-          margin: 0;
-        }
+    .category {
+      color: ${({ theme }) => theme.color.greyLight};
+      font-size: 1em;
+      @media ${({ theme }) => theme.breakpoint.sm} {
+        font-size: 1.5em;
       }
     }
   }
-  .excerpt {
-    font-family: ${({ theme }) => theme.font.copy.name}, sans-serif;
-    font-weight: ${({ theme }) => theme.font.copy.light};
-    font-size: 1.5em;
-    line-height: 1.25em;
-    margin: 0 0 20px 0;
-    text-align: center;
+  .image {
+    display: flex;
+    flex-direction: column;
+    @media ${({ theme }) => theme.breakpoint.md} {
+      box-sizing: border-box;
+      flex-direction: row;
+      margin: 0 0 20px 0;
+      width: 100%;
+    }
+    .left {
+      display: flex;
+      flex-direction: column;
+      @media ${({ theme }) => theme.breakpoint.md} {
+        box-sizing: border-box;
+        width: 66%;
+      }
+      .content {
+        color: ${({ theme }) => theme.color.greyLight};
+        margin: 0 auto;
+        max-width: ${({ theme }) => theme.width.article};
+        padding: 20px;
+        h2,
+        h3,
+        h4,
+        h5 {
+          color: ${({ theme }) => theme.color.black};
+          margin: 0 0 20px 0;
+        }
+      }
+    }
+    .right {
+      width: 100%;
+      @media ${({ theme }) => theme.breakpoint.md} {
+        background-color: ${({ theme }) => theme.color.white};
+        box-sizing: border-box;
+        display: block;
+        height: 100%;
+        position: sticky;
+        position: -webkit-sticky;
+        top: ${({ theme }) => theme.height.navBarScrolled};
+        width: 33%;
+      }
+    }
   }
 `
 
-export default function PressRelease({ pressRelease }) {
-  if (!pressRelease) {
+const HighlightsStyled = styled.div`
+  padding: 20px 20px 30px 20px;
+  @media ${({ theme }) => theme.breakpoint.md} {
+    font-size: 1.5em;
+    margin: 0;
+    padding: 0 20px;
+  }
+  p {
+    color: ${({ theme }) => theme.color.black};
+    font-weight: ${({ theme }) => theme.font.copy.bold};
+  }
+  ul li {
+    color: ${({ theme }) => theme.color.greyLight};
+  }
+`
+
+const ImageStyled = styled.div`
+  background-color: ${({ theme }) => theme.color.black};
+  background-image: url('${(props) => props.imageUrl}');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 350px;
+  @media ${({ theme }) => theme.breakpoint.md} {
+    height: 500px;
+  }
+`
+
+export default function PressRelease({ caseStudy }) {
+  if (!caseStudy) {
     return <></>
   }
 
-  if (!pressRelease.author) {
+  if (!caseStudy.title) {
     return <ErrorPage statusCode={404} />
   }
 
   return (
     <PageStyled>
       <Head>
-        <title>{pressRelease.title} | EQ Works</title>
+        <title>{caseStudy.title} | EQ Works</title>
       </Head>
-      <TempPageTopPadding />
-      <Link href='/press-releases'>
-        <a className='press-releases-link'>Press Releases</a>
-      </Link>
-      <Article>
-        <h1>{pressRelease.title}</h1>
-        <p className='excerpt'>{pressRelease.excerpt}</p>
-        <p className='author-date'>
-          {pressRelease.author.fields.name} /{' '}
-          <Date className='date' dateString={pressRelease.date} />
+      <div className='header'>
+        <h1>{caseStudy.title}</h1>
+        <p className='category'>
+          Case Study:{' '}
+          {caseStudy.categories.map((category, index) => {
+            return <span key={index}>{category.fields.title}</span>
+          })}
         </p>
-        <ReadingTime data={pressRelease.content.content} />
-        <ArticleContent content={pressRelease.content} />
-      </Article>
+      </div>
+      <div className='image'>
+        <div className='left'>
+          <ImageStyled imageUrl={caseStudy.featuredImage.fields.file.url} />
+          <div className='content'>
+            <ArticleContent content={caseStudy.content} />
+          </div>
+        </div>
+        <div className='right'>
+          <HighlightsStyled>
+            <p>Highlights</p>
+            <ul>
+              {caseStudy.highlights.map((highlight, index) => {
+                return <li key={index}>{highlight}</li>
+              })}
+            </ul>
+          </HighlightsStyled>
+        </div>
+      </div>
     </PageStyled>
   )
 }
 
 export async function getStaticProps({ params }) {
-  const pressRelease = await getEntryBySlug('post', params.slug)
+  const caseStudy = await getEntryBySlug('caseStudy', params.slug)
 
   return {
     props: {
-      pressRelease
+      caseStudy
     }
   }
 }
 
 export async function getStaticPaths() {
-  const allPressReleases = await getEntries('post', '4cuZTcGorM9T6djiI3JQ8l')
+  const allPressReleases = await getEntries('caseStudy')
 
   const slugArray = []
   allPressReleases.map((item) => {
