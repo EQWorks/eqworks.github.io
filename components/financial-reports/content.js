7536e353958137e303fdb9d1bf8ee6fddfa12acc
Sam Sverko <sam.sverko@gmail.com>
2020-09-09 15:01:40 -0400

    Financial Reports - Style list of documents.

--- components/financial-reports/content.js
+++ components/financial-reports/content.js
@@ -6,19 +6,22 @@ import UnorderedList from './unordered-list'
 
 const SectionStyled = styled.section`
   color: ${({ theme }) => theme.color.greyLight};
+  display: flex;
+  flex-wrap: wrap;
   margin: 0 auto;
   max-width: ${({ theme }) => theme.width.article};
   padding: 40px 20px 40px 20px;
-  h2 {
-    color: ${({ theme }) => theme.color.black};
-    font-size: 1.75em;
-    margin: 0 0 20px 0;
+  @media ${({ theme }) => theme.breakpoint.xs} {
+    justify-content: flex-start;
   }
 `
 
 export default function Content({ financialReports }) {
   const dtrOptions = {
     renderNode: {
+      [BLOCKS.HR]: (node) => {
+        return null
+      },
       [BLOCKS.UL_LIST]: (node) => {
         return <UnorderedList data={node.content} />
       }
