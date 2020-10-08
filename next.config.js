74d284351f6e23fd85411eed148e357028bb049b
Sam Sverko <sam.sverko@gmail.com>
2020-10-08 16:06:56 -0400

    Next - Remove assetPrefix & basePath

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
-  assetPrefix: isProd ? '/cs-company-website' : '',
-  basePath: isProd ? '/cs-company-website' : '',
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
