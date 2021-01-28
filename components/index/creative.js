4347474857f5519cb2a9fe71f4fadcd911f51a1c
Sam Sverko <sam.sverko@gmail.com>
2021-01-28 15:49:56 -0500

    Index - Creative - Fix styling.

--- components/index/creative.js
+++ components/index/creative.js
@@ -1,44 +1,98 @@
-import styled from 'styled-components'
+import styled, { useTheme } from 'styled-components'
+import * as React from 'react'
 
 import Button from '../shared/button'
 
 const SectionStyled = styled.section`
-  border: 1px solid red;
   color: ${({ theme }) => theme.color.black};
   font-size: ${(props) => (props.largeFont ? '2em' : '1em')};
   text-align: center;
+  margin: 0 auto;
+  max-width: ${({ theme }) => theme.width.page};
   padding: 0 ${({ theme }) => theme.spacing[2]}px;
   h2 {
-    margin: 0 auto ${({ theme }) => theme.spacing[2]}px auto;
+    margin: 0 auto ${({ theme }) => theme.spacing[3]}px auto;
+    @media ${({ theme }) => theme.breakpoint.md} {
+      margin: 0 0 ${({ theme }) => theme.spacing[3]}px 0;
+    }
   }
-  @media ${({ theme }) => theme.breakpoint.sm} {
-    /* display: none; */
+  @media ${({ theme }) => theme.breakpoint.md} {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    text-align: left;
   }
   .left {
+    margin: 0 auto ${({ theme }) => theme.spacing[5]}px auto;
+    @media ${({ theme }) => theme.breakpoint.md} {
+      margin: 0;
+      padding: 0 ${({ theme }) => theme.spacing[2]}px 0 0;
+      /* width: 40%; */
+    }
     .divider {
       background-color: ${({ theme }) => theme.color.blue};
       height: 5px;
-      margin: 0 auto ${({ theme }) => theme.spacing[2]}px auto;
+      margin: 0 auto ${({ theme }) => theme.spacing[3]}px auto;
       width: 100px;
       @media ${({ theme }) => theme.breakpoint.md} {
-        margin: 0 0 ${({ theme }) => theme.spacing[2]}px 0;
+        margin: 0 0 ${({ theme }) => theme.spacing[3]}px 0;
       }
     }
     .subtitle {
       font-size: 1.15em;
-      margin: 0 auto ${({ theme }) => theme.spacing[2]}px auto;
+      margin: 0 auto ${({ theme }) => theme.spacing[3]}px auto;
     }
     .text {
-      margin: 0 auto ${({ theme }) => theme.spacing[2]}px auto;
+      margin: 0 auto ${({ theme }) => theme.spacing[3]}px auto;
+    }
+    .subtitle, .text {
+      max-width: 500px;
+      @media ${({ theme }) => theme.breakpoint.md} {
+        margin: 0 0 ${({ theme }) => theme.spacing[3]}px 0;
+        /* max-width: 400px; */
+      }
+    }
+  }
+  .right {
+    @media ${({ theme }) => theme.breakpoint.md} {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+      padding: 0;
+    }
+    img {
+      max-width: 500px;
+      width: 100%;
+      @media ${({ theme }) => theme.breakpoint.md} {
+        max-width: 100%;
+      }
     }
   }
 `
 
 export default function Creative() {
+  const theme = useTheme()
+  const [width, setWidth] = React.useState(null);
+
+  React.useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize()
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
   return (
     <SectionStyled>
       <div className='left'>
-        <h2>Award-Winning Omni-Channel Creative</h2>
+        {width < theme.breakpointValue.md && (
+          <h2>Award-Winning Omni-Channel Creative</h2>
+        )}
+        {width >= theme.breakpointValue.md && (
+          <h2>Award-Winning<br />Omni-Channel Creative</h2>
+        )}
         <div className='divider' />
         <p className='subtitle'>
           A Canadian-based Creative Studio here to help cut
@@ -48,7 +102,7 @@ export default function Creative() {
           Our in-house studio includes bespoke custom ads on campaigns that
           develop positive moments and build brand&nbsp;loyalty.
         </p>
-        <Button href='/contact' text='EQ Studio' />
+        <Button href='/contact' text='Contact us' />
       </div>
       <div className='right'>
         <img
