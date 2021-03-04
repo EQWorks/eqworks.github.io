8e87452a905bfa9eeb6f01a683c044bce29a8f5e
Sam Sverko <sam.sverko@gmail.com>
2021-03-04 10:53:03 -0500

    Index - Brands - Update border styling.

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ const Hero = dynamic(() => import('../components/index/hero'))
 const AITools = dynamic(() => import('../components/index/ai-tools'))
 const BlueTabs = dynamic(() => import('../components/index/blue-tabs'))
 const EQAdvantage = dynamic(() => import('../components/index/eq-advantage'))
-const Creative = dynamic(() => import('../components/index/creative'))
 const Brands = dynamic(() => import('../components/index/brands'))
 
 export default function Index() {
@@ -14,7 +13,6 @@ export default function Index() {
       <AITools />
       <BlueTabs />
       <EQAdvantage />
-      <Creative />
       <Brands />
     </>
   )
