908432bf917dfc1e40079694dc850a21dec9930b
Sam Sverko <sam.sverko@gmail.com>
2021-03-10 10:15:34 -0500

    CaseStudy - Update blockquote styling.

--- components/shared/blockquote.js
+++ components/shared/blockquote.js
@@ -1,53 +1,70 @@
 import styled from 'styled-components'
 import { QuoteLeft } from '@styled-icons/fa-solid/QuoteLeft'
+import { QuoteRight } from '@styled-icons/fa-solid/QuoteRight'
 
-const BlockquoteStyled = styled.blockquote`
-  background-color: ${({ theme }) => theme.color.greyBorder};
-  border-left: 5px solid ${({ theme }) => theme.color.navBarLinksListHover};
+const DivStyled = styled.div`
+  align-items: stretch;
   display: flex;
-  margin: 0;
-  padding: 20px;
-  .left-side {
-    display: none;
-    min-width: 25px;
-    width: 25px;
-    @media ${({ theme }) => theme.breakpoint.xs} {
-      display: block;
-    }
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: ${({ theme }) => theme.spacing[8]}px 0 0 0;
+  width: 100%;
+  .side {
+    box-sizing: border-box;
+    width: 10%;
     svg {
-      color: ${({ theme }) => theme.color.navBarLinksListHover};
-      width: 100%;
+      color: ${({ theme }) => theme.color.greyLight};
+      max-width: 35px;
+    }
+    &:first-child {
+      align-items: flex-start;
+      display: none;
+      justify-content: flex-end;
+      padding: 0 ${({ theme }) => theme.spacing[2]}px 0 0;
+      @media ${({ theme }) => theme.breakpoint.xs} {
+        display: flex;
+      }
+    }
+    &:last-child {
+      align-items: flex-end;
+      display: none;
+      justify-content: flex-start;
+      padding: 0 0 0 ${({ theme }) => theme.spacing[2]}px;
+      @media ${({ theme }) => theme.breakpoint.sm} {
+        display: flex;
+      }
     }
   }
-  .right-side {
-    @media ${({ theme }) => theme.breakpoint.xs} {
-      padding: 0 0 0 20px;
-      width: calc(100% - 25px);
+  .center {
+    box-sizing: border-box;
+    text-align: left;
+    width: 90%;
+    @media ${({ theme }) => theme.breakpoint.sm} {
+      width: 80%;
     }
-    p {
-      color: ${({ theme }) => theme.color.black};
+    @media ${({ theme }) => theme.breakpoint.md} {
+      max-width: 800px;
+    }
+    blockquote {
+      border: 3px solid ${({ theme }) => theme.color.blue};
       margin: 0;
-      &:first-child {
-        font-style: italic;
-        margin: 0 0 10px 0;
-      }
-      &:last-child {
-        font-weight: ${({ theme }) => theme.font.bold};
-      }
+      padding: ${({ theme }) => theme.spacing[3]}px;
     }
   }
 `
 
 export default function Blockquote({ data }) {
   return (
-    <BlockquoteStyled>
-      <div className='left-side'>
+    <DivStyled>
+      <div className='side'>
         <QuoteLeft aria-label='left quote' role='img' />
       </div>
-      <div className='right-side'>
-        {data[0] && <p>{data[0].content[0].value}</p>}
-        {data[1] && <p>{data[1].content[0].value}</p>}
+      <div className='center'>
+        <blockquote>{data[0].content[0].value}</blockquote>
       </div>
-    </BlockquoteStyled>
+      <div className='side'>
+        <QuoteRight aria-label='right quote' role='img' />
+      </div>
+    </DivStyled>
   )
 }
